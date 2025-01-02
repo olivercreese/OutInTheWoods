94ffa187dec0e50dce552429875c45a821229dff
Oliver Creese <olicreese@gmail.com>
2025-01-02 19:17:35 +0000

    added other sheep type to gameManager spawner

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,16 +7,34 @@ public class GameManager : MonoBehaviour
 
     List<GameObject> animals = new List<GameObject>();
 
-    [SerializeField] GameObject sheepPrefab;
+    [SerializeField] GameObject blacksheepPrefab;
+    [SerializeField] GameObject whitesheepPrefab;
+    [SerializeField] GameObject goatPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         for (int i = 0; i < 10; i++)
         {
-            GameObject sheep = Instantiate(sheepPrefab, new Vector3(Random.Range(100, 400), 12, Random.Range(100, 400)), Quaternion.identity);
+            int random = Random.Range(1, 4);
+            GameObject animalPrefab;
+            switch (random)
+            {
+                case 1:
+                    animalPrefab = Instantiate(whitesheepPrefab, new Vector3(Random.Range(100, 400), 12, Random.Range(100, 400)), Quaternion.identity);
+                    break;
+                case 2:
+                    animalPrefab = Instantiate(blacksheepPrefab, new Vector3(Random.Range(100, 400), 12, Random.Range(100, 400)), Quaternion.identity);
+                    break;
+                case 3:
+                    animalPrefab = Instantiate(goatPrefab, new Vector3(Random.Range(100, 400), 12, Random.Range(100, 400)), Quaternion.identity);
+                    break;
+                default:
+                    animalPrefab = Instantiate(whitesheepPrefab, new Vector3(Random.Range(100, 400), 12, Random.Range(100, 400)), Quaternion.identity);
+                    break;
+            }
             //sheep.GetComponent<Sheep>().player = GameObject.Find("Player");
-            animals.Add(sheep);
+            animals.Add(animalPrefab);
         }
     }
 
